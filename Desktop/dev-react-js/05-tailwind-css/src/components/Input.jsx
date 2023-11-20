c9f42163eec2fbda7ba349f8e63552c72ed9272a
deepakmaven-2610 <deepak.singh@mavenwave.com>
2023-11-20 15:09:57 +0530

    Tailwind CSS Commit

---
+++ Desktop/dev-react-js/05-tailwind-css/src/components/Input.jsx
@@ -0,0 +1,21 @@
+export default function Input({ label, invalid, ...props }) {
+    let labelClass = "block mb-2 text-xs font-bold tracking-wide uppercase";
+    let inputClass = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow"
+    if(invalid)
+    {
+        labelClass += " text-red-400";  //when submiting with empty email and password it will change email and password color to red
+        inputClass += " text-red-500 bg-red-100 border-red-300";
+    }
+    else
+    {
+        labelClass += " text-stone-300";
+        inputClass += " text-gray-700 bg-stone-300";
+    }
+    
+    return (
+      <p>
+        <label className={labelClass}> {label} </label>
+        <input className={inputClass} {...props} />
+      </p>
+    );
+  }
